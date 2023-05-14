780025c196b78c5f60807994fe8d91bdadc2dabf
Kush <kushalxgaikwad@gmail.com>
2023-05-14 13:02:03 +0530

    made files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGptAiModule } from './chat-gpt-ai/chat-gpt-ai.module';
 import { ConfigModule } from '@nestjs/config';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [ChatGptAiModule, ConfigModule.forRoot()],
+  imports: [ChatGptAiModule, ConfigModule.forRoot(), NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
